01a299965736bc10a187e3f2f630facd6b9baa7b
Jürgen Wohlgenannt <juergen@wohlgenannt.info>
2020-06-20 09:51:56 +0200

    Fin single question

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButtonLable: UIButton!
+    @IBOutlet weak var falseButtonLabel: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        guard let userAnswer = sender.currentTitle else { return }
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        updateUI() // to set new score
+        
+        userGotItRight == true ? (sender.backgroundColor = .green) : (sender.backgroundColor = .red)
+        quizBrain.switchNextQuestion()
+
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        trueButtonLable.backgroundColor = UIColor.clear
+        falseButtonLabel.backgroundColor = UIColor.clear
+    }
+}
+
---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Jürgen Wohlgenannt on 19.06.20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        self.text = q
+        self.answer = a
+    }
+}
